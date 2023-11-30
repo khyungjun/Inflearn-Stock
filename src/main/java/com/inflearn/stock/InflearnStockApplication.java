e95d2c054e82aa996103da5ae538d3a163022c6c
khyungjun <kpos7503@naver.com>
2023-12-01 00:02:51 +0900

    Init Project

---
+++ src/main/java/com/inflearn/stock/InflearnStockApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnStockApplication.class, args);
+	}
+
+}
