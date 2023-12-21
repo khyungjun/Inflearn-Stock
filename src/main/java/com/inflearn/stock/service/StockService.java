8e076ecb78e27cea2809da126e72569c534afba6
khyungjun <kpos7503@naver.com>
2023-12-21 18:28:11 +0900

    update project

--- src/main/java/com/inflearn/stock/service/StockService.java
+++ src/main/java/com/inflearn/stock/service/StockService.java
@@ -1,10 +1,10 @@
 package com.inflearn.stock.service;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.inflearn.stock.domain.Stock;
 import com.inflearn.stock.repository.StockRepository;
-
 
 @Service
 public class StockService {
@@ -17,13 +17,9 @@ public class StockService {
 	
 	@Transactional
 	public void decrease(Long id, Long quantity) {
-		// Stock 조회
-		// 재고를 감소시킨 뒤
-		// 갱신된 값을 저장
-		
-		Stock stock = stockRepository.findById(id).orElseThrow();
-		stock.decrease(quantity);
-		stockRepository.saveAndFlush(stock);
+		Stock stock = stockRepository.findById(id).orElseThrow(); // Stock 조회
+		stock.decrease(quantity); // 재고를 감소시킨 뒤
+		stockRepository.saveAndFlush(stock); // 갱신된 값을 저장
 	}
 	
 //	//@Transactional
