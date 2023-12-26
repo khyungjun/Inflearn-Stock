ec048e8183fc51b3632e49e6023a28822a135be2
khyungjun <kpos7503@naver.com>
2023-12-26 22:30:08 +0900

    Update Project

--- src/main/java/com/inflearn/stock/service/StockService.java
+++ src/main/java/com/inflearn/stock/service/StockService.java
@@ -24,12 +24,8 @@ public class StockService {
 	
 //	//@Transactional
 //	public synchronized void decrease(Long id, Long quantity) { 
-//		// Stock 조회
-//		// 재고를 감소시킨 뒤
-//		// 갱신된 값을 저장
-//		
-//		Stock stock = stockRepository.findById(id).orElseThrow();
-//		stock.decrease(quantity);
-//		stockRepository.saveAndFlush(stock);
+//		Stock stock = stockRepository.findById(id).orElseThrow(); // Stock 조회
+//		stock.decrease(quantity); // 재고를 감소시킨 뒤
+//		stockRepository.saveAndFlush(stock); // 갱신된 값을 저장
 //	}
 }
